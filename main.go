aa7750896775046ef65461d1cdadf2b2a200c488
Kevin Visscher <k.visscher@diract-it.nl>
2014-11-21 11:03:31 +0100

    Run gofmt

--- main.go
+++ main.go
@@ -1,32 +1,32 @@
 package main
 
 import (
-    "flag"
-    "net/http"
-    "strconv"
-    "log"
-    "os"
-    "github.com/kvisscher/cloudy-haystack/transform"
-  )
+	"flag"
+	"github.com/kvisscher/cloudy-haystack/transform"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
 
 func main() {
-  var targetHost string
-  var bindPort int
+	var targetHost string
+	var bindPort int
 
-  flag.StringVar(&targetHost, "target", "http://somehost.com", "The host that the message should be re-posted to as JSON")
-  flag.IntVar(&bindPort, "port", 8080, "The port to bind on to receive requests")
-  flag.Parse()
+	flag.StringVar(&targetHost, "target", "http://somehost.com", "The host that the message should be re-posted to as JSON")
+	flag.IntVar(&bindPort, "port", 8080, "The port to bind on to receive requests")
+	flag.Parse()
 
-  logFile, err := os.OpenFile("log.txt", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+	logFile, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 
-  if err != nil {
-    log.Fatal("Was unable to open log file", err)
-  }
+	if err != nil {
+		log.Fatal("Was unable to open log file", err)
+	}
 
-  defer logFile.Close()
+	defer logFile.Close()
 
-  log.SetOutput(logFile)
+	log.SetOutput(logFile)
 
-  http.HandleFunc("/update", transform.UpdateHandler)
-  http.ListenAndServe(":" + strconv.Itoa(bindPort), nil)
+	http.HandleFunc("/update", transform.UpdateHandler)
+	http.ListenAndServe(":"+strconv.Itoa(bindPort), nil)
 }
--- transform/transform.go
+++ transform/transform.go
@@ -1,43 +1,43 @@
 package transform
 
 import (
-    "net/http"
-    "encoding/json"
-    "bytes"
-    "encoding/base64"
-    "io/ioutil"
-    "log"
-  )
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 type RequestJsonWrapper struct {
-  Content string
+	Content string
 }
 
 func UpdateHandler(writer http.ResponseWriter, request *http.Request) {
-  defer request.Body.Close()
+	defer request.Body.Close()
 
-  log.Println("Received request on", request.RequestURI, "from", request.RemoteAddr)
+	log.Println("Received request on", request.RequestURI, "from", request.RemoteAddr)
 
-  body, err := ioutil.ReadAll(request.Body)
+	body, err := ioutil.ReadAll(request.Body)
 
-  if err != nil {
-    log.Println("Was unable to read the request body")
-    return
-  }
+	if err != nil {
+		log.Println("Was unable to read the request body")
+		return
+	}
 
-  writer.WriteHeader(http.StatusOK)
+	writer.WriteHeader(http.StatusOK)
 
-  var buf bytes.Buffer
+	var buf bytes.Buffer
 
-  encoder := base64.NewEncoder(base64.StdEncoding, &buf)
-  encoder.Write(body) // Write the original body of the request
-  encoder.Close()
+	encoder := base64.NewEncoder(base64.StdEncoding, &buf)
+	encoder.Write(body) // Write the original body of the request
+	encoder.Close()
 
-  wrapper := RequestJsonWrapper{Content: buf.String()}
+	wrapper := RequestJsonWrapper{Content: buf.String()}
 
-  jsonEncoder := json.NewEncoder(writer)
-  jsonEncoder.Encode(wrapper)
-  encoder.Close()
+	jsonEncoder := json.NewEncoder(writer)
+	jsonEncoder.Encode(wrapper)
+	encoder.Close()
 
-  log.Println("Send response")
+	log.Println("Send response")
 }
